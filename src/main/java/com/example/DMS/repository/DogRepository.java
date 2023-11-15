09dae30b19931c62a903e6708a7837e4fb16c8b0
Sri Harshika Sattor <123045916+sriharshika@users.noreply.github.com>
2023-11-14 20:53:38 -0600

    final commit

---
+++ src/main/java/com/example/DMS/repository/DogRepository.java
@@ -0,0 +1,14 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Dog;
+
+/***@author  SriHarshika Sattor
+ * 
+***/
+
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+	
+
+}
